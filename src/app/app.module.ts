52453510ec2911328cf210786614fecd9a52f34d
GambrinusNL <114574994+GambrinusNL@users.noreply.github.com>
2024-02-20 22:19:54 +0100

    wishlist added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BeerlistComponent } from './beerlist/beerlist.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BeerlistComponent
+    BeerlistComponent,
+    WishlistComponent
   ],
   imports: [
     BrowserModule,
