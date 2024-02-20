444866f1eace9b66a8d3dd5899f674fb8dff12ac
GambrinusNL <114574994+GambrinusNL@users.noreply.github.com>
2024-02-20 23:09:38 +0100

    router working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BeerlistComponent } from './beerlist/beerlist.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'beerlist', component: BeerlistComponent},
+  { path: 'wishlist', component: WishlistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
