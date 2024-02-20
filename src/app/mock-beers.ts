da9f8d5ae682221a9fd575571f67510efff95bc9
GambrinusNL <114574994+GambrinusNL@users.noreply.github.com>
2024-02-20 16:27:21 +0100

    Base of beerapp build

---
+++ src/app/mock-beers.ts
@@ -0,0 +1,9 @@
+import { Beer } from './beer';
+
+export const HEROES: Beer[] = [
+  { id: 0, name: 'Trammelantje' },
+  { id: 1, name: 'Pure Alcohol Beer' },
+  { id: 2, name: 'Dr. Death' },
+  { id: 3, name: 'Beerbasto' },
+  { id: 4, name: 'ComaBeer' }
+];
