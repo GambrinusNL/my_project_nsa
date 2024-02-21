dfb129fd2ed3431984e779b6ef3fa162d8cf55c1
GambrinusNL <114574994+GambrinusNL@users.noreply.github.com>
2024-02-21 18:55:51 +0100

    test

--- src/app/mock-beers.ts
+++ src/app/mock-beers.ts
@@ -5,5 +5,5 @@ export const BEERS: Beer[] = [
   { id: 1, name: 'Pure Alcohol Beer' },
   { id: 2, name: 'Dr. Death' },
   { id: 3, name: 'Beerbasto' },
-  { id: 4, name: 'ComaBeer' }
+  { id: 4, name: 'ComaaBeer' }
 ];
