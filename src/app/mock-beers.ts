e2a2e078f2511e7a7d275a8f573930e93c4852ec
GambrinusNL <114574994+GambrinusNL@users.noreply.github.com>
2024-02-22 12:00:43 +0100

    Update mock-beers.ts

--- src/app/mock-beers.ts
+++ src/app/mock-beers.ts
@@ -6,5 +6,7 @@ export const BEERS: Beer[] = [
   { id: 2, name: 'Dr. Death' },
   { id: 3, name: 'Beerbasto' },
   { id: 4, name: 'ComaBeer' },
-  { id: 5, name: 'GitBeers' }
+  { id: 5, name: 'GitBeers' },
+  { id: 6, name: 'WierBier' }
+  
 ];
