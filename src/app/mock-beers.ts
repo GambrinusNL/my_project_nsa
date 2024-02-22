91bc8e8341f0eaed88298fed4ef7727ee48c9d03
GambrinusNL <114574994+GambrinusNL@users.noreply.github.com>
2024-02-22 14:09:11 +0100

    test

--- src/app/mock-beers.ts
+++ src/app/mock-beers.ts
@@ -1,13 +1,13 @@
 import { Beer } from './beer';
 
 export const BEERS: Beer[] = [
-  { id: 0, name: 'Trammelantje' },
-  { id: 1, name: 'Pure Alcohol Beer' },
-  { id: 2, name: 'Dr. Death' },
-  { id: 3, name: 'Beerbasto' },
-  { id: 4, name: 'ComaBeer' },
-  { id: 5, name: 'GitBeers' },
-  { id: 6, name: 'SierBier' }
+  { id: 0, name: 'bla - Trammelantje' },
+  { id: 1, name: 'bla - Pure Alcohol Beer' },
+  { id: 2, name: 'bla - Dr. Death' },
+  { id: 3, name: 'bla - Beerbasto' },
+  { id: 4, name: 'bla - ComaBeer' },
+  { id: 5, name: 'bla - GitBeers' },
+  { id: 6, name: 'bla - SierBier' }
   
 ];
 
